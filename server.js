7c635a960254928d279f3bf4cd47f05d197275f8
Priya Gupta <priyagupta052k@gmail.com>
2024-11-17 12:19:07 +0530

    Login-Page

---
+++ server.js
@@ -0,0 +1,188 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+// Initialize Express app
+const app = express();
+
+// Middleware
+const corsOptions = {
+    // React frontend address
+    origin: 'http://localhost:3000',  
+    methods: ['GET', 'POST'],
+};
+app.use(cors(corsOptions));
+
+
+ // To parse JSON body
+app.use(express.json());
+
+const URL = "mongodb+srv://pg0019:admin@cluster0.cfqew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// MongoDB connection
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error(err));
+
+// MongoDB User model
+const User = mongoose.model('loginInfo', new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    number:{ type: String, required: true, unique: true },
+    regNo: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+}));
+
+// const Menu = mongoose.model('fooditem', new mongoose.Schema({
+//     name: String,
+//     price: Number,
+//     rating: Number,
+//     reviews: Number,
+//     description: String,
+//     image: String
+// }));
+// const Menu = mongoose.model('Menu', VitSapaaduSchema);
+
+// const Cafe = mongoose.model('Cafe', new mongoose.Schema({
+//     name: String,
+//     foodTypes: [String],
+// }));
+
+
+// app.get('/api/cafes', async (req, res) => {
+//     try {
+//         const cafes = await Cafe.find();
+//         res.json(cafes);
+//     } catch (error) {
+//         console.error('Error fetching cafes:', error);
+//         res.status(500).json({ error: 'Error fetching cafes' });
+//     }
+// });
+
+
+// Assuming Express and Mongoose setup is already done
+
+app.get('/api/user/:email', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.params.email });
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).send("Error retrieving user data");
+    }
+});
+
+
+// Route to get all food items
+// app.get('/api/items', async (req, res) => {
+//     try {
+//         const items = await Menu.find();
+//         res.json(items);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send("Server Error");
+//     }
+// });
+
+// // Add a new food item (optional, for testing purposes)
+// app.post('/api/items', async (req, res) => {
+//     try {
+//         const { name, price, rating, reviews, description, image } = req.body;
+//         const newItem = new Menu({ name, price, rating, reviews, description, image });
+//         await newItem.save();
+//         res.status(201).json(newItem);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send("Server Error");
+//     }
+// });
+
+
+// app.post('/api/items', (req, res) => {
+//     const { name, price, rating, reviews, description, image } = req.body;
+
+//     const newFoodItem = new Menu({
+//         name,
+//         price,
+//         rating,
+//         reviews,
+//         description,
+//         image
+//     });
+
+//     newFoodItem.save()
+//         .then((foodItem) => {
+//             res.status(201).json({
+//                 message: 'Food item added successfully!',
+//                 foodItem
+//             });
+//         })
+//         .catch((error) => {
+//             res.status(500).json({
+//                 message: 'Error adding food item.',
+//                 error: error.message
+//             });
+//         });
+// });
+
+
+// Route for user registration
+app.post('/register', async (req, res) => {
+    const { name, email,number, regNo, password } = req.body;
+
+    try {
+        // Check if the registration number already exists
+        const userExists = await User.findOne({ regNo });
+        if (userExists) {
+            return res.status(400).json({ error: 'Reg. No. already exists' });
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user
+        const newUser = new User({ name, email, number,regNo, password: hashedPassword });
+        await newUser.save();
+
+        return res.status(201).json({ message: 'User registered successfully' });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Server error' });
+    }
+});
+
+// Route for user login
+app.post('/login', async (req, res) => {
+    const { regNo, password } = req.body;
+
+    try {
+        // Find the user by registration number
+        const user = await User.findOne({ regNo });
+        if (!user) {
+            return res.status(400).json({ error: 'Invalid credentials' });
+        }
+
+        // Check if password matches
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ error: 'Invalid credentials' });
+        }
+
+        // Generate JWT token
+        const token = jwt.sign({ userId: user._id }, "your_jwt_secret", { expiresIn: '1h' });
+
+        return res.status(200).json({ message: 'Login successful', token });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Server error' });
+    }
+});
+
+// Start the Express server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/pages/Login SignUp/LoginSignUp.jsx
@@ -0,0 +1,205 @@
+import React, { useState } from "react";
+// import { useNavigate } from 'react-router-dom';
+import './LoginSignUp.css';
+import { FaUser, FaLock, FaEnvelope, FaRegAddressCard } from "react-icons/fa";
+
+const LoginSignUp = () => {
+    const [isActive, setIsActive] = useState(false);
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        number:'',
+        regNo: '',
+        password: '',
+    });
+    const [error, setError] = useState("");
+    // const navigate = useNavigate();
+
+    const handleRegisterClick = () => {
+        setIsActive(true);
+    };
+
+    const handleLoginClick = () => {
+        setIsActive(false);
+    };
+
+    const handleLoginSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:5000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    regNo: formData.regNo,
+                    password: formData.password,
+                }),
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                localStorage.setItem('token', result.token); // Store the JWT token
+                alert(result.message);  // Show success message
+                // navigate('/home');  // Redirect to Home page
+            } else {
+                setError(result.error || "Login failed");
+            }
+        } catch (err) {
+            setError("An error occurred. Please try again.");
+            console.error(err);
+        }
+    };
+
+    const handleRegisterSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:5000/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: formData.name,
+                    email: formData.email,
+                    number:formData.number,
+                    regNo: formData.regNo,
+                    password: formData.password,
+                }),
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                alert(result.message);  // Show success message
+                setIsActive(false); // Switch to login form after successful registration
+            } else {
+                setError(result.error || "Registration failed");
+            }
+        } catch (err) {
+            setError("An error occurred. Please try again.");
+            console.error(err);
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    return (
+        <div className={`wrapper ${isActive ? 'active' : ''}`}>
+
+            <span className="rotate-bg"></span>
+            <span className="rotate-bg2"></span>
+
+            {/* Login Form */}
+            <div className="form-box login">
+                <h2 className="title animation" style={{ "--i": 0, "--j": 20 }}>Login</h2>
+                <form onSubmit={handleLoginSubmit}>
+                    <div className="input-box animation" style={{ "--i": 1, "--j": 21 }}>
+                        <input
+                            type="text"
+                            required
+                            name="regNo"
+                            value={formData.regNo}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="regNo">Reg. No.</label>
+                        <FaRegAddressCard className="icon" />
+                    </div>
+                    <div className="input-box animation" style={{ "--i": 2, "--j": 22 }}>
+                        <input
+                            type="password"
+                            required
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="password">Password</label>
+                        <FaLock className="icon" />
+                    </div>
+                    {error && <p className="error">{error}</p>}
+                    <button type="submit" className="btn animation" style={{ "--i": 3, "--j": 23 }}>Login</button>
+                    <div className="linkTxt animation" style={{ "--i": 4, "--j": 24 }}>
+                        <p>Don't have an account? <a href="#" onClick={handleRegisterClick} className="register-link">Sign Up</a></p>
+                    </div>
+                </form>
+            </div>
+
+            {/* Registration Form */}
+            <div className="form-box register">
+                <h2 className="title animation" style={{ "--i": 17, "--j": 0 }}>Sign Up</h2>
+                <form onSubmit={handleRegisterSubmit}>
+                    <div className="input-box animation" style={{ "--i": 18, "--j": 1 }}>
+                        <input
+                            type="text"
+                            required
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="name">Name</label>
+                        <FaUser className="icon" />
+                    </div>
+                    <div className="input-box animation" style={{ "--i": 19, "--j": 2 }}>
+                        <input
+                            type="email"
+                            required
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="email">Email</label>
+                        <FaEnvelope className="icon" />
+                    </div>
+                    <div className="input-box animation" style={{ "--i": 19, "--j": 2 }}>
+                        <input
+                            type="number"
+                            required
+                            name="number"
+                            value={formData.number}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="number">Number</label>
+                        <FaEnvelope className="icon" />
+                    </div>
+                    <div className="input-box animation" style={{ "--i": 20, "--j": 3 }}>
+                        <input
+                            type="text"
+                            required
+                            name="regNo"
+                            value={formData.regNo}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="regNo">Reg. No.</label>
+                        <FaRegAddressCard className="icon" />
+                    </div>
+                    <div className="input-box animation" style={{ "--i": 21, "--j": 4 }}>
+                        <input
+                            type="password"
+                            required
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="password">Password</label>
+                        <FaLock className="icon" />
+                    </div>
+                    {error && <p className="error">{error}</p>}
+                    <button type="submit" className="btn animation" style={{ "--i": 23, "--j": 6 }}>Sign Up</button>
+                    <div className="linkTxt animation" style={{ "--i": 24, "--j": 7 }}>
+                        <p>Already have an account? <a href="#" onClick={handleLoginClick} className="login-link">Login</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginSignUp;

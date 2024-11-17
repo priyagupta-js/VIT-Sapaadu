2f6be776ef9a7aaeeebcc06dd44b787b2f6d276f
Priya Gupta <priyagupta052k@gmail.com>
2024-11-17 14:43:13 +0530

    Added NavBar

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import mainlogo from '../../assets/logo.png';
+import './NavBar.css';
+
+
+function NavBar(){
+    const navigate = useNavigate();
+    const handleAccountClick = () => {
+        navigate(`/account`);
+    };
+    const handleCartClick = () => {
+        navigate(`/cart`);
+    };
+    return (
+        <div className='navbar'>
+            <div className='navleft'>
+                <img src={mainlogo} alt="LOGO" className='navlogo' />
+                <h2>VIT Sapaadu</h2>
+            </div>
+            <div className='navright'>
+                <ul className='navoptions'>
+                    <li><a href="#Search">Search</a></li>
+                    <li onClick={handleAccountClick}><a href="#Account">Account</a></li>
+                    <li onClick={handleCartClick}><a href="#Cart">Cart</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+
+export default NavBar;
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import  NavBar from '../../components/NavBar/Navbar.jsx'
+function Homepage()
+{
+    return(
+        <NavBar />
+    );
+}
+
+export default Homepage;

c8334029614a662365de5e36405fc7b2019f8f5e
Priya Gupta <priyagupta052k@gmail.com>
2024-11-17 14:56:00 +0530

    Added Footer

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import  NavBar from '../../components/NavBar/Navbar.jsx'
+import  NavBar from '../../components/NavBar/Navbar.jsx';
+import Footer from "../../components/footer/Footer.jsx";
 function Homepage()
 {
     return(
-        <NavBar />
+        <>
+            <NavBar />
+            <Footer />
+        </>
+       
     );
 }
 
